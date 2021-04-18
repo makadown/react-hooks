18ecfcf88f0355aee5b04f7b7b946699ce841195
makadown <maseflores79@gmail.com>
2021-04-18 08:09:45 -0500

    pruebas con useCounter

---
+++ src/tests/hooks/useCounter.test.js
@@ -0,0 +1,51 @@
+import {renderHook, act} from '@testing-library/react-hooks';
+import {useCounter} from '../../hooks/useCounter';
+
+describe('Tests en Hook de useCounter', () => {
+    
+    test('Debe retornar valores por defecto ', () => {
+        
+        const {result} = renderHook( () => useCounter() );
+        expect(result.current.state).toBe(10);
+        expect(typeof result.current.increment).toBe('function');
+        expect(typeof result.current.decrement).toBe('function');
+        expect(typeof result.current.reset).toBe('function');
+        
+    });
+
+    test('Debe tener counter en 100 ', () => {
+        const {result} = renderHook( () => useCounter(100) );
+        expect(result.current.state).toBe(100);
+    });
+
+
+    test('Debe incrementar counter en 1', () => {
+        const {result} = renderHook( () => useCounter(100) );
+        const {increment} = result.current;
+        act( () => {
+            increment(1);
+        });
+        expect(result.current.state).toBe(101);
+    });
+    
+    test('Debe decrementar counter en 1', () => {
+        const {result} = renderHook( () => useCounter(100) );
+        const {decrement} = result.current;
+        act( () => {
+            decrement(1);
+        });
+        expect(result.current.state).toBe(99);
+    });
+
+    test('Debe resetear counter en predeterminado (100)', () => {
+        const {result} = renderHook( () => useCounter(100) );
+        const {increment, reset} = result.current;
+        act( () => {
+            increment(4);
+            reset();
+        });
+        expect(result.current.state).toBe(100);
+    });
+
+    
+});
